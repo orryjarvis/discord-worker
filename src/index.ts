c127205c427e657dae6e645dd4e8eacb1b93ff0b
Orry A. Jarvis <oajarvis@sep.com>
2022-05-21 10:04:03 -0400

    skeleton worker; ci; test; lint

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,37 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { Router } from 'itty-router';
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKey
+} from 'discord-interactions';
+
+const router = Router();
+
+router.get('/', (request, env) => {
+  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
 
 export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(request: Request, env: any): Promise<Response> {
+    if (request.method === 'POST') {
+      const signature = request.headers.get('x-signature-ed25519') ?? "";
+      const timestamp = request.headers.get('x-signature-timestamp') ?? "";
+      console.log(signature, timestamp, env.DISCORD_PUBLIC_KEY);
+      const body = await request.clone().arrayBuffer();
+      const isValidRequest = verifyKey(
+        body,
+        signature,
+        timestamp,
+        env.DISCORD_PUBLIC_KEY
+      );
+      if (!isValidRequest) {
+        console.error('Invalid Request');
+        return new Response('Bad request signature.', { status: 401 });
+      }
+    }
+    return router.handle(request, env);
   },
 };
