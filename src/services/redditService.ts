50603f7b59f587cd9d2cc23fc340066167c91f29
Orry Jarvis <oajarvis@sep.com>
2025-08-19 22:01:53 -0400

    reddit user agent

--- src/services/redditService.ts
+++ src/services/redditService.ts
@@ -6,7 +6,7 @@ export const redditService = {
   async getMedia(subreddit: string): Promise<string> {
     const response = await fetch(`https://www.reddit.com/r/${subreddit}/hot.json`, {
       headers: {
-        'User-Agent': 'justinbeckwith:awwbot:v1.0.0 (by /u/justinblat)',
+        'User-Agent': 'mrmeseeks:discordbot:v1.0.0 (by /u/twiitchz)',
       },
     });
     const data: any = await response.json();
