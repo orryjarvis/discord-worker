42bd243dff3f6c86dd27872e1369ced9494d0958
Orry Jarvis <oajarvis@sep.com>
2025-08-25 10:56:11 -0400

    more secrets stuff

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -23,7 +23,7 @@ async function signAndSendRequest(body: object): Promise<Response> {
   const timestamp = Date.now().toString();
   const json = JSON.stringify(body);
   const message = new TextEncoder().encode(timestamp + json);
-  const privateKeyHex = process.env.DISCORD_PRIVATE_KEY || "bfbb3f0da095ff411191941cf30bc7a4afaa2a223b78ad05a049751b265f5049";
+  const privateKeyHex = process.env.SIGNATURE_PRIVATE_KEY || "bfbb3f0da095ff411191941cf30bc7a4afaa2a223b78ad05a049751b265f5049";
   const privateKey = Uint8Array.from(Buffer.from(privateKeyHex, "hex"));
   const signatureUint8 = await ed.signAsync(message, privateKey);
   const signatureHex = Buffer.from(signatureUint8).toString('hex');
