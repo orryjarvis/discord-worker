a98df25bd03d49d9cf204e92f5bf836730448497
Orry Jarvis <oajarvis@sep.com>
2025-08-27 15:58:09 -0400

    set vars for test and dev environments

--- test/e2e/setup.shared.ts
+++ test/e2e/setup.shared.ts
@@ -10,7 +10,7 @@ async function signRequest(body: object): Promise<SimpleRequestInit> {
   const timestamp = Date.now().toString();
   const json = JSON.stringify(body);
   const message = new TextEncoder().encode(timestamp + json);
-  const privateKeyHex = process.env.SIGNATURE_PRIVATE_KEY || "bfbb3f0da095ff411191941cf30bc7a4afaa2a223b78ad05a049751b265f5049";
+  const privateKeyHex = process.env.SIGNATURE_PRIVATE_KEY || "d46b224eca160429fbbd3c903994bb93da0532635839530a1fd6cdac1bd4023e";
   const privateKey = Uint8Array.from(Buffer.from(privateKeyHex, "hex"));
   const signatureUint8 = await ed.signAsync(message, privateKey);
   const signatureHex = Buffer.from(signatureUint8).toString('hex');
