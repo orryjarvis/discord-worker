deb5d74878e3c658ef901b1dd17bbda0012f5637
Orry Jarvis <oajarvis@sep.com>
2025-08-28 13:35:49 -0400

    git ignore generated files

--- types/swagger2openapi.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'swagger2openapi' {
-  export function convertObj(input: any, options?: any): Promise<{ openapi: any }>; 
-  const _default: { convertObj: typeof convertObj };
-  export default _default;
-}
